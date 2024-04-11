141c244283bd2ef7d782915b707e047a3bcdb75b
HungTran <hunghuutran009@gmail.com>
2024-04-11 16:02:00 +0700

    write js drum kit

--- js/drum-kit/js/drumKit.js
+++ js/drum-kit/js/drumKit.js
@@ -1,7 +1,7 @@
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return; // stop the funtion from running all together  
+    if (!audio) return; // stop the function from running all together  
     audio.currentTime = 0;  // rewind to the start
     audio.play();
     key.classList.add('playing');
@@ -12,6 +12,13 @@ function removeTransition(e) {
     this.classList.remove("playing");
 }
 
+const sound = document.querySelectorAll(".sound");
+sound.forEach((item) => {
+    const text = item.textContent.toUpperCase();
+    item.innerHTML = text;
+});
+
 const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener("keydown", playSound);
+
